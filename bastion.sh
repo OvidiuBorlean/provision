ee6583d1b7045e3a859504d6a7574920176d575d
Ovidiu Borlean <ovidiu.borlean@gmail.com>
2022-04-25 17:49:02 +0300

    Update bastion.sh

--- bastion.sh
+++ bastion.sh
@@ -1,4 +1,20 @@
 
 #!/bin/bash
-echo "Applying Configuration"
-echo "ovidiu" > /tmp/ovidiu
+echo "Applying Custom Configuration"
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+    
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+   
+sudo apt-get update
+sudo apt-get install azure-cli
+
+echo "Install kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo mv ./kubectl /usr/bin/kubectl
